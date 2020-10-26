22a25c43ee9548f5c8ebe519328177aa7484ca85
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-26 18:23:17 -0300

    Versionando a api!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    end    
   end
 end
