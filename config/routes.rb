c1482cb429da0205539899edf5e45c0b21c3caea
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-27 11:26:14 -0300

    Criando um model User !

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
     namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
