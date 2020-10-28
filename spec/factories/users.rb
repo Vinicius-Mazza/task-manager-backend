a39f885b50930bc00076852c6700d05c619b76c6
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-10-28 10:43:42 -0300

    Testes automatizados no model user !

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password "3301"
+    password_confirmation "3301"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,7 +46,7 @@ RSpec.configure do |config|
     end
   end
 
-  consfig.include FactoryGirl::Syntax::Methods
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
