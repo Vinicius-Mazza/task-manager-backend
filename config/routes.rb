dd20d7bb6ff396935099df446225ca34e348f05d
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-11-14 10:45:47 -0300

    Adicionando ação update no controle de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update]
     end    
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -32,6 +32,7 @@ RSpec.describe 'Users API', type: :request do
     end
   end
 
+
   describe 'POST /users' do
     before do
       headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
@@ -65,4 +66,35 @@ RSpec.describe 'Users API', type: :request do
     end
   end
 
+
+  describe 'PUT /users/:id' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      put "/users/#{user_id}", params: { user: user_params }, headers: headers 
+    end
+
+    context 'when the requests params are valid' do
+      let(:user_params) { { email: 'new_email@taskmanager.com' } }
+      it 'status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the json data for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the requests params are invalid' do
+      let(:user_params) { { email: 'invalid_email@' } }
+      it 'status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the erros' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
 end
