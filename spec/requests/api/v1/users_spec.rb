4796c74bbc53223b715e32f20e7ceb296e97f712
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-11-14 08:15:53 -0300

    Ajustando diretorio de trabalho

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,29 +4,29 @@ RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
 
-  before { host! "api.taskmanager.test" }
+  before { host! 'api.taskmanager.test' }
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.taskmanager.v1" }
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
-    context "when the user exists" do
-      it "returns the user" do
+    context 'when the user exists' do
+      it 'returns the user' do
         user_response = JSON.parse(response.body)
-        expect(user_response["id"]).to eq(user_id)
+        expect(user_response['id']).to eq(user_id)
       end
 
-      it "return status code 200" do
+      it 'return status code 200' do
         expect(response).to have_http_status(200)
       end 
     end
 
-    context "when the user does not exists" do
+    context 'when the user does not exists' do
       let(:user_id) { 10000 }
 
-      it "returns status code 404" do
+      it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
     end
