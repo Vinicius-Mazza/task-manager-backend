c87b26b2a2bbb2c2c70d68f1b7f00e170b539e20
Vinicius Mazza <viniciusmazza2552@gmail.com>
2020-12-21 11:38:09 -0300

    Adicionando a action create no controle de tarefas(tasks) com testes

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,20 @@ class Api::V1::TasksController < ApplicationController
     task = current_user.tasks.find(params[:id])
     render json: task, status: 200
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+
+    if task.save
+      render json: task, status: 201
+    else
+      render json: { errors: task.errors }, status: 422
+    end
+  end
+
+  private 
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :done)
+  end
 end
